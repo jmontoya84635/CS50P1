8062c31b87448b06338cfd49e18ad838b79eab7f
juanmontoya <juanmontoya940@gmail.com>
2024-01-19 12:06:54 -0600

    finished the create new page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.searchResult, name="searchResult"),
-    path("search", views.searchBar, name="searchBar")
+    path("search", views.searchBar, name="searchBar"),
+    path("addPage/", views.addPage, name="addPage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,30 @@
+import os.path
+
 from django.shortcuts import render
-
+from django import forms
 from . import util
+from django.http import HttpResponseRedirect
 
 
+class WikiPage(forms.Form):
+    title = forms.CharField(label="Title", widget=forms.TextInput(attrs={
+        "class": "form-control",
+    }))
+    content = forms.CharField(label="Content", widget=forms.Textarea(attrs={
+        "class": "form-control",
+        "rows": 4,
+    }))
+
+
+# Home page
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
+
     })
 
 
+# Page that shows up when editing url
 def searchResult(request, title):
     # Checks if there is a result for search
     result = util.get_entry(title)
@@ -25,6 +41,7 @@ def searchResult(request, title):
     })
 
 
+# Page that loads when querying with "q"
 def searchBar(request):
     # Grabs the q parameter from the get request and does the same as previous func
     search = request.GET.get('q')
@@ -47,4 +64,30 @@ def searchBar(request):
         "foundRelated": False if (len(subStringEntries) == 0) else True,
         "relatedEntries": subStringEntries,
 
+    })
+
+
+# Page where user can create new page
+def addPage(request):
+    if request.method == "POST":
+        form = WikiPage(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            allEntries = util.list_entries()
+            for entry in allEntries:
+                if title.lower() == entry.lower():
+                    print("trying to warn user")
+                    return render(request, "encyclopedia/addPage.html", {
+                        "form": form,
+                        "alreadyExist": True
+                    })
+            filepath = os.path.join('entries', f'{title}.md')
+            print(filepath)
+            newFile = open(filepath, "x")
+            newFile.write("# "+title+"\n\n")
+            newFile.write(content)
+            return HttpResponseRedirect(f'/wiki/{title}')
+    return render(request, "encyclopedia/addPage.html", {
+        "form": WikiPage(),
     })
